84ae50ca4361d7f1d2309a0056e542af3b61f734
cindyxinyiwang <xwang29@nd.edu>
2016-04-21 22:19:15 -0400

    EM algorithm

--- PCFG/EM/em.py
+++ PCFG/EM/em.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import math
 
 class GrammarGen():
 	def __init__(self, alp):
@@ -17,20 +19,24 @@ class GrammarGen():
 				rules.append(n + " " + i)
 
 		for n in self.alphabet:
-			self.rule_dict[n] = self._get_rules(rules)
+			if n == self.start:
+				self.rule_dict[n] = self._get_rules(rules, nonterm=False)
+			else:
+				self.rule_dict[n] = self._get_rules(rules, nonterm=False)
 
 	def printAllRules(self):
 		for s in self.rule_dict:
 			for r in self.rule_dict[s]:
 				print s + "->" + r + " " + str(self.rule_dict[s][r])
 
-	def _get_rules(self, rules):
+	def _get_rules(self, rules, nonterm=True):
 		"""
 		input: list of all rules
 		output: dictionary of rules and random probs
 		"""
 		# append a terminal rule
-		rules.append("a")
+		if not nonterm:
+			rules.append("a")
 		n = len(rules)
 		a = np.random.random(n)
 		a /= a.sum()
@@ -42,6 +48,7 @@ class GrammarGen():
 		return d
 
 
+
 class EM():
 	def __init__(self, gram):
 		"""
@@ -49,7 +56,7 @@ class EM():
 		"""
 		self.grammar = gram
 		self.len_distrib = {}
-		self.distrib_len = 10
+		self.distrib_len = 200
 		self.emp_count = {}
 		self._get_emp_count("../real_data/sec02-21.words")
 		self.loglikelihood = []
@@ -57,6 +64,9 @@ class EM():
 		self.beta = {}
 		self.expect = {}
 		self.maximize = {}
+
+		self.initLenDistrib(normalize=False)
+		self.getLogLikelihood()
 
 	def _get_emp_count(self, filename):
 		with open(filename) as myfile:
@@ -71,17 +81,20 @@ class EM():
 	def printLenDistrib(self):
 		print self.len_distrib
 
-	def initLenDistrib(self):
+	def initLenDistrib(self, normalize=True):
 		# get terminal symbol probabilities
+		self.len_distrib = {}
 		for s in self.grammar.rule_dict:
 			rules = self.grammar.rule_dict[s]
+			self.len_distrib[s] = [0, 0]
 			for r in rules:
 				l = r.split()
 				if len(l) == 1:
-					if s in self.len_distrib:
-						self.len_distrib[s][0] += rules[r]
+					if len(self.len_distrib[s]) == 2:
+						self.len_distrib[s][1] += rules[r]
 					else:
-						self.len_distrib[s] = [0, rules[r]]
+						self.len_distrib[s].append(rules[r])
+
 		for l in range(self.distrib_len):
 			l += 2
 			for k in self.len_distrib:
@@ -93,77 +106,162 @@ class EM():
 					if len(rules) == 2:
 						tmp = 0
 						for i in range(l-1):
-							tmp += self.len_distrib[rules[0]][i+1] * self.len_distrib[rules[1]][l-i-1]
+							i += 1
+							tmp += self.len_distrib[rules[0]][i] * self.len_distrib[rules[1]][l-i]
 						sum_prob += prob * tmp
 				self.len_distrib[k].append(sum_prob)
+		if (normalize):
+			print "normalize"
+			for r in self.len_distrib:
+				rules = self.len_distrib[r]
+				sum_r = 0
+				for p in rules:
+					sum_r += p
+				for i in range(len(rules)):
+					self.len_distrib[r][i] = self.len_distrib[r][i]/sum_r
 
 	def getLogLikelihood(self):
 		log_result = 0
 		for i in range(self.distrib_len):
-			# len_distrib item i is the distrib of string length i+1
+			i += 1	# start from length 1
 			if i in self.emp_count:
 				log_result += self.emp_count[i] * np.log(self.len_distrib[self.grammar.start][i])
 		self.loglikelihood.append(log_result)
 
 	def getBeta(self):
+		# outside probability
+		self.beta = {}
 		for s in self.grammar.alphabet:
-			self.beta[s] = [1]
+				self.beta[s] = [0]
+		self.beta[self.grammar.start][0] = 1
+
 		for i in range(self.distrib_len):
 			i += 1
 			for x in self.grammar.alphabet:
 				r_rules = self.grammar.rule_dict[x]
 				for r in r_rules:
+					prob = self.grammar.rule_dict[x][r]
 					rules = r.split()
 					if len(rules) == 2:
 						z = rules[0]
 						y = rules[1]
-						if len(self.beta[z]) == (i+1):
+						if len(self.beta[z]) >= (i+1):
+							tmp = 0
 							for k in range(i):
-								self.beta[z][i] += self.beta[z][k] * self.len_distrib[y][i-k]
+								tmp += self.beta[x][k] * self.len_distrib[y][i-k] * prob
+							self.beta[z][i] += tmp 
 						else:
+							tmp = 0
 							for k in range(i):
-								self.beta[z].append(self.beta[z][k] * self.len_distrib[y][i-k])
+								tmp += self.beta[x][k] * self.len_distrib[y][i-k] * prob
+							self.beta[z].append(tmp )
 
 						y = rules[0]
 						z = rules[1]
-						if len(self.beta[z]) == (i+1):
+						if len(self.beta[z]) >= (i+1):
+							tmp = 0
 							for k in range(i):
-								self.beta[z][i] += self.beta[z][k] * self.len_distrib[y][i-k]
+								tmp += self.beta[x][k] * self.len_distrib[y][i-k] * prob
+							self.beta[z][i] += tmp 
 						else:
+							tmp = 0
 							for k in range(i):
-								self.beta[z].append(self.beta[z][k] * self.len_distrib[y][i-k])
+								tmp += self.beta[x][k] * self.len_distrib[y][i-k] * prob
+							self.beta[z].append(tmp)
 
 	def expectation(self):
+		self.expect = {}
 		for x in self.grammar.rule_dict:
 			r_rules = self.grammar.rule_dict[x]
 			for r in r_rules:
 				rules = r.split()
 				prob = r_rules[r]
 				e = 0
-				if len(rules == 2):
+				if len(rules) == 2:
 					for n in range(self.distrib_len):
+						n += 1
+						if n not in self.emp_count:
+							continue
 						tmp = 0
-						for k in range(n):
-							for l in range(k):
-								for m in range(l):
-									tmp += self.beta[x][k]*prob*self.len_distrib[rules[0]][l]*self.len_distrib[rules[1]][m]
-						e += tmp * self.emp_count[n] / self.len_distrib[self.start][n]
+						for k in range(n+1):
+							for l in range(n-k):
+								m = n - k - l
+								tmp += self.beta[x][k]*prob*self.len_distrib[rules[0]][l]*self.len_distrib[rules[1]][m]
+						e += tmp * self.emp_count[n] / self.len_distrib[self.grammar.start][n]
 				else:
-					pass
-				if x in self.expect:
-					self.expect[x][r_rules] = e
-				else:
-					self.expect[x] = {r_rules: e}
+					for n in range(self.distrib_len):
+						n += 1
+						if n not in self.emp_count:
+							continue
+						tmp = self.emp_count[n] * self.beta[x][n-1]*prob / self.len_distrib[self.grammar.start][n]
+						e += tmp
 
+				if x in self.expect:
+					self.expect[x][r] = e
+				else:
+					self.expect[x] = {}
+					self.expect[x][r] = e
+
+	def maximization(self):
+		for x in self.grammar.rule_dict:
+			r_rules = self.grammar.rule_dict[x]
+			r_expects = self.expect[x]
+			sum_x = 0
+			for s in r_expects:
+				sum_x += r_expects[s]
+			for r in r_rules:
+				self.grammar.rule_dict[x][r] = r_expects[r]/sum_x
+
+		#self.grammar.printAllRules()
+		#print self.expect
+
+	def check(self):
+		# calculate beta(a, n)
+		self.beta_a = {}
+		for x in self.grammar.rule_dict:
+			r_rules = self.grammar.rule_dict[x]
+			for r in r_rules:
+				rules = r.split()
+				prob = r_rules[r]
+				if len(rules) == 1:
+					a = rules[0]
+					if a not in self.beta_a:
+						self.beta_a[a] = [0 for i in range(self.distrib_len)]
+					for i in range(self.distrib_len):
+						if i == 0:
+							continue
+						self.beta_a[a][i] += self.beta[x][i-1] * prob
+		sum_distrib = 0
+		for i in self.len_distrib[self.grammar.start]:
+			sum_distrib += i
+
+		print "start prob sum: " + str(sum_distrib)
+		print self.len_distrib[self.grammar.start][:10]
+		print self.beta_a['a'][:10]
+		print self.beta[self.grammar.start][:30]
+
+	def iteration(self, max_iter, bound):
+		last_log = self.loglikelihood
+
+		for i in range(max_iter):
+			last_log = self.loglikelihood[i]
+			self.getBeta()
+			self.expectation()
+			self.maximization()
+			self.initLenDistrib(normalize=False)
+			self.getLogLikelihood()
+			print self.loglikelihood
+			#self.check()
+			#self.grammar.printAllRules()
+			#print self.expect
+			if (math.fabs(self.loglikelihood[i+1] - last_log) < bound):
+				break
 
 if __name__ == "__main__":
-	gram = GrammarGen(['A', 'B', 'C' ,'D', 'E'])
+	gram = GrammarGen(['A', 'B' ])
 	gram.getAllRules()
-	gram.printAllRules()
+	#gram.printAllRules()
 
+	
 	em = EM(gram)
-	em.initLenDistrib()
-	em.printLenDistrib()
-	em.getLogLikelihood()
-	print em.loglikelihood
-	em.getBeta()
+	em.iteration(50, 0)
--- xwang29-cp3/sent_generator.py
+++ xwang29-cp3/sent_generator.py
@@ -1,6 +1,59 @@
 #from numpy.random import choice
 from random import random
 from bisect import bisect
+import random as rd
+from nltk.tokenize import word_tokenize
+import sys
+
+class markov:
+	def __init__(self, train_file="", level=2):
+		self.model = {}
+		self.n = level
+		self.text = self.readtraining(train_file)
+		self.tokens = word_tokenize(self.text)
+		self.build_model_word()
+		
+
+	def readtraining(self, filepath):
+		with open(filepath, "r") as file:
+			return file.read()
+
+	def build_model_word(self):
+		n = self.n
+		model = dict()
+		#tokens = self.text.strip().split(" ")
+		if len(self.tokens) < n:
+			return model
+		for i in range(len(self.tokens) - n):
+			gram = tuple(self.tokens[i:i+n])
+			next_token = self.tokens[i+n]
+			if gram in model:
+				model[gram].append(next_token)
+			else:
+				model[gram] = [next_token]
+		final_gram = tuple(self.tokens[len(self.tokens)-n:])
+		if final_gram in model:
+			model[final_gram].append(None)
+		else:
+			model[final_gram] = [None]
+		self.model = model
+
+	def generate(self,seed=None, max_iterations=20):
+		n = self.n
+		if seed is None:
+			seed = rd.choice(self.model.keys())
+		output = list(seed)
+		current = tuple(seed)
+		for i in range(max_iterations):
+			if current in self.model:
+				possible_next_tokens = self.model[current]
+				next_token = rd.choice(possible_next_tokens)
+				if next_token is None: break
+				output.append(next_token)
+				current = tuple(output[-n:])
+			else:
+				break
+		return ' '.join(output)
 
 class sent_generator:
 	def __init__(self, gram_file="eliminated.grammar", lex_file="out.txt.lexicon"):
@@ -85,6 +138,16 @@ class sent_generator:
 
 if __name__=="__main__":
 	gen = sent_generator()
-	num = 2
-	for i in num:
-		print gen.generate("ROOT_0")
+	mar = markov("train.txt")
+	while True:
+		choice = raw_input("Please select: q to quit, p for generate with PCFG grammar, m for generate with Markov chain")
+		if choice == 'q':
+			sys.exit(1)
+		if choice == 'p':
+			print gen.generate("ROOT_0")
+			continue
+		if choice == 'm':
+			print mar.generate()
+		else:
+			print "Not recognized!"
+
